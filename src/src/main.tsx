78bc17c4a65a31f41fb8cc116b00ca1312a32d8f
root <fraz666@blu.it>
2023-02-19 17:24:36 +0000

    testing intregration between preact and three

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,41 +1,6 @@
 import { render } from 'preact'
-import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+
 import { App } from './app'
 import './index.css'
 
-// render(<App />, document.getElementById('app') as HTMLElement)
-
-const scene = new Scene();
-const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-
-//   stats.begin();
-
-  // monitored code goes here
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-
-//   stats.end();
-
-  requestAnimationFrame(animate);
-}
-
-animate();
+render(<App />, document.getElementById('app') as HTMLElement)
